61682bb38dfb4b104db9a53db479a52309044960
xianzhedong <xianzhedong@tencent.com>
2026-04-04 13:43:57 +0800

    refine example.

--- example.py
+++ example.py
@@ -2,13 +2,25 @@ import sys
 sys.path.append('third_party/Matcha-TTS')
 from cosyvoice.cli.cosyvoice import AutoModel
 import torchaudio
+from datetime import datetime
+
+CN_DIGITS = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十',
+             '十一', '十二', '十三', '十四', '十五', '十六', '十七', '十八', '十九', '二十',
+             '二十一', '二十二', '二十三', '二十四', '二十五', '二十六', '二十七', '二十八', '二十九', '三十',
+             '三十一', '三十二', '三十三', '三十四', '三十五', '三十六', '三十七', '三十八', '三十九', '四十',
+             '四十一', '四十二', '四十三', '四十四', '四十五', '四十六', '四十七', '四十八', '四十九', '五十',
+             '五十一', '五十二', '五十三', '五十四', '五十五', '五十六', '五十七', '五十八', '五十九']
+
+def get_time_text() -> str:
+    now = datetime.now()
+    return f'现在的时间是{CN_DIGITS[now.hour]}点{CN_DIGITS[now.minute]}分。'
 
 
 def cosyvoice3_example():
-    """ CosyVoice3 Usage, check https://funaudiollm.github.io/cosyvoice3/ for more details
-    """
     cosyvoice = AutoModel(model_dir='/data/home/xianzhedong/models/Fun-CosyVoice3-0.5B')
-    for i, j in enumerate(cosyvoice.inference_zero_shot('今天天气真好，能陪我出去逛逛吗？', 'You are a helpful assistant.<|endofprompt|>希望你以后能够做的比我还好呦。',
+    tts_text = get_time_text()
+    print(f'TTS text: {tts_text}')
+    for i, j in enumerate(cosyvoice.inference_zero_shot(tts_text, 'You are a helpful assistant.<|endofprompt|>希望你以后能够做的比我还好呦。',
                                                         './asset/zero_shot_prompt.wav', stream=False)):
         torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
 
