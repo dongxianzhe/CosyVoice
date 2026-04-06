5baaa0935097d672fa3620b6990871d80deb3432
xianzhedong <xianzhedong@tencent.com>
2026-04-06 22:34:25 +0800

    s.

--- cosyvoice/utils/class_utils.py
+++ cosyvoice/utils/class_utils.py
@@ -1,28 +1,6 @@
-# Copyright [2023-11-28] <sxc19@mails.tsinghua.edu.cn, Xingchen Song>
-#            2024 Alibaba Inc (authors: Xiang Lyu)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import torch
 
-from cosyvoice.transformer.activation import Swish
-from cosyvoice.transformer.subsampling import (
-    LinearNoSubsampling,
-    EmbedinigNoSubsampling,
-    Conv1dSubsampling2,
-    Conv2dSubsampling4,
-    Conv2dSubsampling6,
-    Conv2dSubsampling8,
-)
+
 from cosyvoice.transformer.embedding import (PositionalEncoding,
                                              RelPositionalEncoding,
                                              WhisperPositionalEncoding,
@@ -31,11 +9,10 @@ from cosyvoice.transformer.embedding import (PositionalEncoding,
 from cosyvoice.transformer.attention import (MultiHeadedAttention,
                                              RelPositionMultiHeadedAttention)
 from cosyvoice.transformer.embedding import EspnetRelPositionalEncoding
-from cosyvoice.transformer.subsampling import LegacyLinearNoSubsampling
 from cosyvoice.llm.llm import CosyVoice3LM
 from cosyvoice.flow.flow import CausalMaskedDiffWithDiT
-from cosyvoice.hifigan.generator import HiFTGenerator, CausalHiFTGenerator
-from cosyvoice.cli.model import CosyVoiceModel, CosyVoice2Model, CosyVoice3Model
+from cosyvoice.hifigan.generator import CausalHiFTGenerator
+from cosyvoice.cli.model import CosyVoice3Model
 
 
 COSYVOICE_ACTIVATION_CLASSES = {
@@ -43,18 +20,10 @@ COSYVOICE_ACTIVATION_CLASSES = {
     "tanh": torch.nn.Tanh,
     "relu": torch.nn.ReLU,
     "selu": torch.nn.SELU,
-    "swish": getattr(torch.nn, "SiLU", Swish),
     "gelu": torch.nn.GELU,
 }
 
 COSYVOICE_SUBSAMPLE_CLASSES = {
-    "linear": LinearNoSubsampling,
-    "linear_legacy": LegacyLinearNoSubsampling,
-    "embed": EmbedinigNoSubsampling,
-    "conv1d2": Conv1dSubsampling2,
-    "conv2d": Conv2dSubsampling4,
-    "conv2d6": Conv2dSubsampling6,
-    "conv2d8": Conv2dSubsampling8,
     'paraformer_dummy': torch.nn.Identity
 }
 
