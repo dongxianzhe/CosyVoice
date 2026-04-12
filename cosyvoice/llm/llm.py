1a34b6804a859744ae42164309188e8fd8ce5a93
xianzhedong <xianzhedong@tencent.com>
2026-04-12 23:17:13 +0800

    simplify llm.

--- cosyvoice/llm/llm.py
+++ cosyvoice/llm/llm.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, List, Generator
+from typing import Callable
 import torch
 from torch import nn, Tensor
 from transformers import Qwen2ForCausalLM
@@ -39,7 +39,7 @@ class CosyVoice3LM(torch.nn.Module):
             sampling: Callable,
             length_normalized_loss: bool = True,
             lsm_weight: float = 0.0,
-            mix_ratio: List[int] = [5, 15],
+            mix_ratio: list[int] = [5, 15],
     ):
         torch.nn.Module.__init__(self)
         self.llm_input_size = llm_input_size
@@ -64,35 +64,34 @@ class CosyVoice3LM(torch.nn.Module):
         self.stop_token_ids = [speech_token_size + i for i in range(200)]
 
     @torch.inference_mode()
-    def inference(self, params: TTSInputParams, sampling: int = 25, max_token_text_ratio: float = 20, min_token_text_ratio: float = 2) -> Generator[torch.Tensor, None, None]:
+    def inference(self, params: TTSInputParams, sampling: int = 25, max_token_text_ratio: float = 20, min_token_text_ratio: float = 2):
         text = torch.concat([params.prompt_text, params.text], dim=1)
-        params.text_len += params.prompt_text_len
+        text_len = params.text_len + params.prompt_text_len
         text_emb = self.llm.model.model.embed_tokens(text)
         assert 151646 in text, '<|endofprompt|> not detected in CosyVoice3 text or prompt_text, check your input!'
 
         sos_emb: Tensor = self.speech_embedding.weight[self.sos].reshape(1, 1, -1)
         task_id_emb: Tensor = self.speech_embedding.weight[self.task_id].reshape(1, 1, -1)
-        if params.llm_prompt_speech_token_len != 0:
-            prompt_speech_token_emb = self.speech_embedding(params.llm_prompt_speech_token)
-        else:
-            prompt_speech_token_emb = torch.zeros(1, 0, self.llm_input_size, dtype=text_emb.dtype)
+        prompt_speech_token_emb = self.speech_embedding(params.llm_prompt_speech_token)
         lm_input: Tensor = torch.concat([sos_emb, text_emb, task_id_emb, prompt_speech_token_emb], dim=1)
 
-        min_len: int = int((params.text_len - params.prompt_text_len) * min_token_text_ratio)
-        max_len: int = int((params.text_len - params.prompt_text_len) * max_token_text_ratio)
-        for token in self.inference_wrapper(lm_input, sampling, min_len, max_len):
-            yield token
-
-    @torch.inference_mode()
-    def inference_wrapper(self, lm_input: Tensor, sampling: int, min_len: int, max_len: int) -> Generator[Any, Any, None]:
-        out_tokens = []
+        min_len: int = int((text_len - params.prompt_text_len) * min_token_text_ratio)
+        max_len: int = int((text_len - params.prompt_text_len) * max_token_text_ratio)
+        out_tokens: list[int] = []
         cache = None
         for i in range(max_len):
-            y_pred, cache = self.llm.forward_one_step(lm_input,
-                                                        masks=torch.tril(torch.ones((1, lm_input.shape[1], lm_input.shape[1]), device=lm_input.device)).to(torch.bool),
-                                                        cache=cache)
+            y_pred, cache = self.llm.forward_one_step(
+                lm_input,
+                masks=torch.tril(torch.ones((1, lm_input.shape[1], lm_input.shape[1]), device=lm_input.device)).to(torch.bool),
+                cache=cache
+            )
             logp = self.llm_decoder(y_pred[:, -1]).log_softmax(dim=-1)
-            top_ids = self.sampling_ids(logp.squeeze(dim=0), out_tokens, sampling, ignore_eos=True if i < min_len else False)
+            top_ids: int = self.sampling_ids(
+                weighted_scores=logp.squeeze(dim=0), 
+                decoded_tokens=out_tokens, 
+                sampling=sampling, 
+                ignore_eos=True if i < min_len else False
+            )
             if top_ids in self.stop_token_ids:
                 break
             yield top_ids
@@ -102,7 +101,7 @@ class CosyVoice3LM(torch.nn.Module):
     def sampling_ids(
         self,
         weighted_scores: torch.Tensor,
-        decoded_tokens: List,
+        decoded_tokens: list[int],
         sampling: int,
         ignore_eos: bool = True,
     ):
