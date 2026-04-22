6feab52b82518afb6714232331533321b1107eb2
xianzhedong <xianzhedong@tencent.com>
2026-04-22 16:50:11 +0800

    simplify dit.

--- cosyvoice/flow/DiT/dit.py
+++ cosyvoice/flow/DiT/dit.py
@@ -1,4 +1,3 @@
-
 """
 ein notation:
 b - batch
@@ -7,16 +6,13 @@ nt - text sequence
 nw - raw wave length
 d - dimension
 """
-
 from __future__ import annotations
-
 import torch
 from torch import nn, Tensor
 import torch.nn.functional as F
 from einops import repeat
 from x_transformers.x_transformers import RotaryEmbedding, apply_rotary_pos_emb
 from cosyvoice.utils.mask import add_optional_chunk_mask
-from typing import Optional
 import math
 
 
@@ -25,7 +21,7 @@ class SinusPositionEmbedding(nn.Module):
         super().__init__()
         self.dim = dim
 
-    def forward(self, x: Tensor, scale=1000) -> Tensor:
+    def forward(self, x: Tensor, scale: float=1000) -> Tensor:
         device = x.device
         half_dim = self.dim // 2
         emb = math.log(10000) / (half_dim - 1)
@@ -36,201 +32,146 @@ class SinusPositionEmbedding(nn.Module):
 
 
 class CausalConvPositionEmbedding(nn.Module):
-    def __init__(self, dim, kernel_size=31, groups=16):
+    def __init__(self, dim: int, kernel_size: int=31, groups: int=16):
         super().__init__()
         assert kernel_size % 2 != 0
-        self.kernel_size = kernel_size
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(dim, dim, kernel_size, groups=groups, padding=0),
+        self.kernel_size: int = kernel_size
+        self.conv1= nn.Sequential(
+            nn.Conv1d(in_channels=dim, out_channels=dim, kernel_size=kernel_size, groups=groups, padding=0),
             nn.Mish(),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv1d(dim, dim, kernel_size, groups=groups, padding=0),
+            nn.Conv1d(in_channels=dim, out_channels=dim, kernel_size=kernel_size, groups=groups, padding=0),
             nn.Mish(),
         )
 
-    def forward(self, x: float["b n d"], mask: bool["b n"] | None = None):  # noqa: F722
-        assert mask is None
-        x = x.permute(0, 2, 1)
-        x = F.pad(x, (self.kernel_size - 1, 0, 0, 0))
-        x = self.conv1(x)
-        x = F.pad(x, (self.kernel_size - 1, 0, 0, 0))
-        x = self.conv2(x)
-        out = x.permute(0, 2, 1)
-        return out
+    def forward(self, x: Tensor, mask: Tensor | None = None) -> Tensor:
+        # x (b, n, d) mask (b, n) d is dim, n is time, b is batch
+        if mask is not None:
+            mask: Tensor = mask[..., None]
+            x = x.masked_fill(~mask, 0.0)
+        x = x.permute(0, 2, 1) # (b, d, n)
+        x = nn.functional.pad(input=x, pad=(self.kernel_size - 1, 0, 0, 0)) # (b, d, n + kernel_size - 1) pad in left
+        x = self.conv1(x) # (b, d, n)
+        x = nn.functional.pad(input=x, pad=(self.kernel_size - 1, 0, 0, 0)) # (b, d, n + kernel_size - 1) pad in left
+        x = self.conv2(x) # (b, d, n)
+        out: Tensor = x.permute(0, 2, 1) # (b, n, d)
+        if mask is not None:
+            out = out.masked_fill(~mask, 0.0)
+        return out # (b, n, d)
+
 
 class AdaLayerNormZero(nn.Module):
-    def __init__(self, dim):
+    def __init__(self, dim: int):
         super().__init__()
         self.silu = nn.SiLU()
-        self.linear = nn.Linear(dim, dim * 6)
-        self.norm = nn.LayerNorm(dim, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(in_features=dim, out_features=dim * 6)
+        self.norm = nn.LayerNorm(normalized_shape=dim, elementwise_affine=False, eps=1e-6)
 
     def forward(self, x: Tensor, emb=None) -> tuple[Tensor, Tensor, Tensor, Tensor, Tensor]:
+        # x (b, n, dim) emb (b, dim)
         emb = self.linear(self.silu(emb))
-        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = torch.chunk(emb, 6, dim=1)
+        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = torch.chunk(input=emb, chunks=6, dim=1) # (b, 6 * dim) -> (b, dim), (b, dim), (b, dim), (b, dim), (b, dim), (b, dim)
         x = self.norm(x) * (1 + scale_msa[:, None]) + shift_msa[:, None]
         return x, gate_msa, shift_mlp, scale_mlp, gate_mlp
 
 
 class AdaLayerNormZero_Final(nn.Module):
-    def __init__(self, dim):
+    def __init__(self, dim: int):
         super().__init__()
         self.silu = nn.SiLU()
-        self.linear = nn.Linear(dim, dim * 2)
-        self.norm = nn.LayerNorm(dim, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(in_features=dim, out_features=dim * 2)
+        self.norm = nn.LayerNorm(normalized_shape=dim, elementwise_affine=False, eps=1e-6)
 
     def forward(self, x: Tensor, emb: Tensor) -> Tensor:
+        # x (b, n, dim) emb (b, dim)
         emb = self.linear(self.silu(emb))
-        scale, shift = torch.chunk(emb, 2, dim=1)
-        x = self.norm(x) * (1 + scale)[:, None, :] + shift[:, None, :]
-        return x
+        scale, shift = torch.chunk(input=emb, chunks=2, dim=1)
+        # scale (b, dim) shift (b, dim)
+        x = self.norm(x) * (1 + scale)[:, None, :] + shift[:, None, :] # (b, n, dim)
+        return x # (b, n, dim)
 
 
 class FeedForward(nn.Module):
-    def __init__(self, dim, dim_out=None, mult=4, dropout=0.0, approximate: str = "none"):
+    def __init__(self, dim: int, mult: int=4):
         super().__init__()
         inner_dim = int(dim * mult)
-        dim_out = dim_out if dim_out is not None else dim
-        activation = nn.GELU(approximate=approximate)
-        project_in = nn.Sequential(nn.Linear(dim, inner_dim), activation)
-        self.ff = nn.Sequential(project_in, nn.Dropout(dropout), nn.Linear(inner_dim, dim_out))
+        self.ff = nn.Sequential(
+            nn.Sequential(
+                nn.Linear(dim, inner_dim), 
+                nn.GELU(approximate="tanh")
+            ), 
+            nn.Dropout(0.1), 
+            nn.Linear(inner_dim, dim), 
+        )
 
     def forward(self, x: Tensor) -> Tensor:
         return self.ff(x)
 
 
 class Attention(nn.Module):
-    def __init__(
-        self,
-        processor: AttnProcessor,
-        dim: int,
-        heads: int = 8,
-        dim_head: int = 64,
-        dropout: float = 0.0,
-        context_dim: Optional[int] = None,  # if not None -> joint attention
-        context_pre_only=None,
-    ):
+    def __init__(self, dim: int, heads: int = 8, dim_head: int = 64):
         super().__init__()
-
-        if not hasattr(F, "scaled_dot_product_attention"):
-            raise ImportError("Attention equires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0.")
-
-        self.processor = processor
-
+        self.dim = dim
         self.heads = heads
         self.inner_dim = dim_head * heads
-        self.dropout = dropout
 
-        self.context_dim = context_dim
-        self.context_pre_only = context_pre_only
+        self.to_q = nn.Linear(in_features=dim, out_features=self.inner_dim)
+        self.to_k = nn.Linear(in_features=dim, out_features=self.inner_dim)
+        self.to_v = nn.Linear(in_features=dim, out_features=self.inner_dim)
 
-        self.to_q = nn.Linear(dim, self.inner_dim)
-        self.to_k = nn.Linear(dim, self.inner_dim)
-        self.to_v = nn.Linear(dim, self.inner_dim)
-
-        if self.context_dim is not None:
-            self.to_k_c = nn.Linear(context_dim, self.inner_dim)
-            self.to_v_c = nn.Linear(context_dim, self.inner_dim)
-            if self.context_pre_only is not None:
-                self.to_q_c = nn.Linear(context_dim, self.inner_dim)
-
-        self.to_out = nn.ModuleList([])
+        self.to_out = nn.ModuleList()
         self.to_out.append(nn.Linear(self.inner_dim, dim))
-        self.to_out.append(nn.Dropout(dropout))
+        self.to_out.append(nn.Dropout(0.1))
 
-        if self.context_pre_only is not None and not self.context_pre_only:
-            self.to_out_c = nn.Linear(self.inner_dim, dim)
-
-    def forward(
-        self,
-        x: float["b n d"],  # noised input x  # noqa: F722
-        c: float["b n d"] = None,  # context c  # noqa: F722
-        mask: bool["b n"] | None = None,  # noqa: F722
-        rope=None,  # rotary position embedding for x
-        c_rope=None,  # rotary position embedding for c
-    ) -> torch.Tensor:
-        if c is not None:
-            return self.processor(self, x, c=c, mask=mask, rope=rope, c_rope=c_rope)
-        else:
-            return self.processor(self, x, mask=mask, rope=rope)
-
-
-class AttnProcessor:
-    def __init__(self):
-        pass
-
-    def __call__(
-        self,
-        attn: Attention,
-        x: float["b n d"],  # noised input x  # noqa: F722
-        mask: bool["b n"] | None = None,  # noqa: F722
-        rope=None,  # rotary position embedding
-    ) -> torch.FloatTensor:
+    def forward(self, x: Tensor, mask: Tensor, rope: Tensor) -> torch.Tensor:
+        # x (b, n, d) mask (b, n) rope (1, n, d)
         batch_size = x.shape[0]
+        query, key, value = self.to_q(x), self.to_k(x), self.to_v(x)
+        freqs, scale = rope
+        query, key = apply_rotary_pos_emb(query, freqs), apply_rotary_pos_emb(key, freqs)
 
-        # `sample` projections.
-        query = attn.to_q(x)
-        key = attn.to_k(x)
-        value = attn.to_v(x)
-
-        # apply rotary position embedding
-        if rope is not None:
-            freqs, xpos_scale = rope
-            q_xpos_scale, k_xpos_scale = (xpos_scale, xpos_scale**-1.0) if xpos_scale is not None else (1.0, 1.0)
-
-            query = apply_rotary_pos_emb(query, freqs, q_xpos_scale)
-            key = apply_rotary_pos_emb(key, freqs, k_xpos_scale)
-
-        # attention
         inner_dim = key.shape[-1]
-        head_dim = inner_dim // attn.heads
-        query = query.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
-        key = key.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
-        value = value.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+        head_dim = inner_dim // self.heads
+        query = query.view(batch_size, -1, self.heads, head_dim).transpose(1, 2) # (b, n, d) -> (b, h, n, d)
+        key = key.view(batch_size, -1, self.heads, head_dim).transpose(1, 2) # (b, n, d) -> (b, h, n, d)
+        value = value.view(batch_size, -1, self.heads, head_dim).transpose(1, 2) # (b, n, d) -> (b, h, n, d)
 
         # mask. e.g. inference got a batch with different target durations, mask out the padding
         if mask is not None:
             attn_mask = mask
             if attn_mask.dim() == 2:
                 attn_mask = attn_mask.unsqueeze(1).unsqueeze(1)  # 'b n -> b 1 1 n'
-                attn_mask = attn_mask.expand(batch_size, attn.heads, query.shape[-2], key.shape[-2])
+                attn_mask = attn_mask.expand(batch_size, self.heads, query.shape[-2], key.shape[-2])
         else:
             attn_mask = None
 
-        x = F.scaled_dot_product_attention(query, key, value, attn_mask=attn_mask, dropout_p=0.0, is_causal=False)
-        x = x.transpose(1, 2).reshape(batch_size, -1, attn.heads * head_dim)
+        x = nn.functional.scaled_dot_product_attention(query, key, value, attn_mask=attn_mask, dropout_p=0.0, is_causal=False)
+        x = x.transpose(1, 2).reshape(batch_size, -1, self.heads * head_dim) # (b, h, n, d) -> (b, n, h * d)
         x = x.to(query.dtype)
-
         # linear proj
-        x = attn.to_out[0](x)
-        # dropout
-        x = attn.to_out[1](x)
+        x = self.to_out[0](x)
 
         if mask is not None:
             if mask.dim() == 2:
-                mask = mask.unsqueeze(-1)
+                mask = mask.unsqueeze(dim=-1)
             else:
-                mask = mask[:, 0, -1].unsqueeze(-1)
+                mask = mask[:, 0, -1].unsqueeze(dim=-1)
             x = x.masked_fill(~mask, 0.0)
-
         return x
 
-# DiT Block
+
 class DiTBlock(nn.Module):
-    def __init__(self, dim, heads, dim_head, ff_mult=4, dropout=0.1):
+    def __init__(self, dim: int, heads: int, dim_head: int, ff_mult: int):
         super().__init__()
         self.attn_norm = AdaLayerNormZero(dim)
-        self.attn = Attention(processor=AttnProcessor(), dim=dim, heads=heads, dim_head=dim_head, dropout=dropout)
+        self.attn = Attention(dim=dim, heads=heads, dim_head=dim_head)
         self.ff_norm = nn.LayerNorm(dim, elementwise_affine=False, eps=1e-6)
-        self.ff = FeedForward(dim=dim, mult=ff_mult, dropout=dropout, approximate="tanh")
+        self.ff = FeedForward(dim=dim, mult=ff_mult)
 
-    def forward(self, x, t, mask=None, rope=None):  # x: noised input, t: time embedding
-        # pre-norm & modulation for attention input
+    def forward(self, x: Tensor, t: Tensor, mask: Tensor, rope: tuple[Tensor, float]) -> Tensor:  # x: noised input, t: time embedding
         norm, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.attn_norm(x, emb=t)
-        # attention
         attn_output = self.attn(x=norm, mask=mask, rope=rope)
-        # process attention output for input x
         x = x + gate_msa.unsqueeze(1) * attn_output
         ff_norm = self.ff_norm(x) * (1 + scale_mlp[:, None]) + shift_mlp[:, None]
         ff_output = self.ff(ff_norm)
@@ -283,15 +224,12 @@ class DiT(nn.Module):
         out_channels: int,
         static_chunk_size: int,
         num_decoding_left_chunks: int, 
-        dropout=0.1, # not set in model_config
     ):
         super().__init__()
         self.time_embed = TimestepEmbedding(dim)
         self.input_embed = InputEmbedding(mel_dim, mu_dim, dim, spk_dim)
         self.rotary_embed = RotaryEmbedding(dim_head)
-        self.transformer_blocks = nn.ModuleList(
-            [DiTBlock(dim=dim, heads=heads, dim_head=dim_head, ff_mult=ff_mult, dropout=dropout) for _ in range(depth)]
-        )
+        self.transformer_blocks = nn.ModuleList([DiTBlock(dim=dim, heads=heads, dim_head=dim_head, ff_mult=ff_mult) for _ in range(depth)])
         self.norm_out = AdaLayerNormZero_Final(dim)  # final modulation
         self.proj_out = nn.Linear(dim, mel_dim)
         self.static_chunk_size = static_chunk_size
